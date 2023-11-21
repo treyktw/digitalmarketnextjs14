9a6ea461685728d04ff2e8fdcb4f0670aaf27a5e
treyktw <101672317+treyktw@users.noreply.github.com>
2023-11-21 02:34:00 -0500

    init commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./maxwidthwrapper";
+import { Icons } from "./icons";
+import NavItems from "./navitems";
+import { buttonVariants } from "./ui/button";
+import Cart from "./cart";
+
+const Navbar = () => {
+
+  const user = null
+
+  return (
+    <div className="bg-whtie sticky z-50 top-0 inset-x-0 h-16 ">
+      <header className="relative bg-whtie">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+
+              {/* Todo Mobile Nav */}
+              <div className="ml-4 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems/>
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (<Link href="/sign-in" className={buttonVariants({variant: "ghost"})}>Sign In</Link>  )}
+                  {user ? null : (<span className="h-6 w-px bg-gray-200" aria-hidden/>)}
+
+                  {user ? <p></p> : (<Link href="/sign-up" className={buttonVariants({variant: "ghost"})}>Create Account</Link>)}
+                  {user ? <span aria-hidden="true" className="h-6 w-px bg-gray-200"></span> : null}
+                  {user ? null : <div className="flex lg:ml-6"><span aria-hidden="true" className="h-6 w-px bg-gray-200"></span></div>}
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/get-payload.ts
@@ -0,0 +1,51 @@
+import dotenv from "dotenv";
+import path from "path";
+import payload, { Payload } from "payload";
+import type { InitOptions } from "payload/config";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../.env")
+});
+
+// caching to safe resources
+let cached = (global as any).payload
+
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  }
+}
+
+
+interface Args {
+  initOptions?: Partial<InitOptions>
+};
+
+export const getPayloadClient = async ({initOptions}: Args = {}):Promise<Payload> => {
+  if(!process.env.PAYLOAD_SECRET) {
+    throw new Error("PAYLOAD_SECRET is Missing")
+  };
+  if(cached.client) {
+    return cached.client
+  }
+
+  if(!cached.promise){
+    cached.promose = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: initOptions?.express ? false : true,
+      ...(initOptions || {}),
+    })
+  }
+
+  try {
+    cached.client = await cached.promise
+  } catch (e: unknown) {
+    cached.promise = null
+    throw e
+  }
+
+  return cached.client
+};
+
+// Created db client to use across 
